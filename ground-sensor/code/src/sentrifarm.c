ff2f423f585815699d13fb64c88a1fb02f8d65ec
Andrew McDonnell <bugs@andrewmcdonnell.net>
2015-03-29 01:52:10 +1030

    Operational hello world for sentrifarm

---
+++ ground-sensor/code/src/sentrifarm.c
@@ -0,0 +1,21 @@
+#include "ets_sys.h"
+#include "os_type.h"
+#include "mem.h"
+#include "osapi.h"
+#include "user_interface.h"
+
+void sentifarm_init()
+{
+  console_printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+  console_printf("~                                         ~\n");
+  console_printf("~ HACKADAY                                ~\n");
+  console_printf("~         PRIZE                           ~\n");
+  console_printf("~              2015                       ~\n");
+  console_printf("~                                         ~\n");
+  console_printf("~                  ENTRY                  ~\n");
+  console_printf("~                       BY                ~\n");
+  console_printf("~                         @pastcompute    ~\n");
+  console_printf("~                                         ~\n");
+  console_printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+}
+
