08eebf619fb3ddd417107606cd2d0fc91bbdd18a
Andrew McDonnell <bugs@andrewmcdonnell.net>
2015-04-05 21:09:22 +0930

    fix typos

--- ground-sensor/code/src/sentrifarm.c
+++ ground-sensor/code/src/sentrifarm.c
@@ -1,3 +1,7 @@
+/*
+ * Main entry and timer for sentrifarm ground sensor.
+ */
+
 #include "ets_sys.h"
 #include "os_type.h"
 #include "mem.h"
@@ -17,6 +21,7 @@
 
 static os_timer_t sensor_poll_timer;
 
+/* Handler for poll timer */
 static void sensor_poll_timer_cb(void*unused)
 {
   struct ds18b20_t results[2];
@@ -50,12 +55,12 @@ static void sensor_poll_timer_cb(void*unused)
 
 
 /* This function is called at the end of the init_done() function installed by user_init() */
-void sentifarm_init()
+void sentrifarm_init()
 {
   /* Setup a timer to read DS18B20 and send a TCP message */
 	const char *s_poll = env_get("sentri-poll");
   os_timer_disarm(&sensor_poll_timer);
-  os_timer_setfn(&sensor_poll_timer,  (os_timer_func_t *)sensor_poll_timer_cb, NULL);
+  os_timer_setfn(&sensor_poll_timer, (os_timer_func_t *)sensor_poll_timer_cb, NULL);
   os_timer_arm(&sensor_poll_timer, (s_poll ? atoi(s_poll) : SENSOR_POLL_INTERVAL_SECONDS)*1000, 1);
 
   console_printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
