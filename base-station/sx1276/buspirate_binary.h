e4e557e3471e950e0bf03fe93a542aaff79d1437
Andrew McDonnell <bugs@andrewmcdonnell.net>
2015-06-01 11:53:55 +0930

    Update file licenses

--- base-station/sx1276/buspirate_binary.h
+++ base-station/sx1276/buspirate_binary.h
@@ -1,3 +1,21 @@
+/*
+  Copyright (c) 2015 Andrew McDonnell <bugs@andrewmcdonnell.net>
+
+  This file is part of SentriFarm SX1276 Relay.
+
+  SentriFarm SX1276 Relay is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  SentriFarm Radio Relay is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with SentriFarm Radio Relay.  If not, see <http://www.gnu.org/licenses/>.
+*/
 #ifndef BUSPIRATE_BINARY_H__
 #define BUSPIRATE_BINARY_H__
 
