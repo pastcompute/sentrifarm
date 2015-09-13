71e95a2e4df6121c0c528b4e5e587ed6cb488e58
Andrew McDonnell <bugs@andrewmcdonnell.net>
2015-09-13 22:23:34 +0930

    more flash strings

--- software/mcu/libraries/sentrifarm/sf-sensordata.cpp
+++ software/mcu/libraries/sentrifarm/sf-sensordata.cpp
@@ -14,7 +14,7 @@ namespace Sentrifarm {
     char buf[256];
     Serial.println(LINE_DOUBLE);
 
-    Serial.print("Software tag ");
+    Serial.print(F("Software tag "));
     Serial.println(sw_version);
 #if defined(ESP8266)
     snprintf(buf, sizeof(buf), "ESP8266: ver=%d boot_mode=%d vcc=%d sdk=%s MAC=%02x:%02x:%02x:%02x:%02x:%02x",
@@ -22,18 +22,18 @@ namespace Sentrifarm {
     Serial.println(buf);
 #endif
 
-    Serial.print("BOOT COUNT : "); Serial.println(bootCount);
+    Serial.print(F("BOOT COUNT : ")); Serial.println(bootCount);
 
     if (have_date) {
       snprintf((char*)buf, sizeof(buf), "DS1307: 20%02u-%02d-%02u %02u-%02u\n\r",
                   (int)year, (int)month, (int)dayOfMonth, (int)hour, (int)minute);
       Serial.print(buf);
-    } else { Serial.println(("DS1307            NOT FOUND")); }
+    } else { Serial.println((F("DS1307            NOT FOUND"))); }
 
     if (have_radio) {
       snprintf((char*)buf, sizeof(buf), "SX1276 Version  0x%02x\n\r", radio_version);
       Serial.print(buf);
-    } else { Serial.println(("Radio             NOT FOUND")); }
+    } else { Serial.println((F("Radio             NOT FOUND"))); }
 
     if (have_bmp180) {
       snprintf((char*)buf, sizeof(buf), "Barometer       %4d.%d hPa\n\rAir temperature  %3d.%d degC\n\rAltitude        %4d.%d\n\r",
@@ -41,7 +41,7 @@ namespace Sentrifarm {
                (int)floorf(ambient_degc), fraction(ambient_degc),
                (int)floorf(altitude_m), fraction(altitude_m));
       Serial.print(buf);
-    } else { Serial.println(("Barometer       NOT FOUND")); }
+    } else { Serial.println((F("Barometer       NOT FOUND"))); }
 
     if (have_pcf8591) {
       int v0 = float(adc_data0) * PCF8591_VREF / 256.F;
@@ -52,7 +52,7 @@ namespace Sentrifarm {
       snprintf((char*)buf, sizeof(buf), "PCF8591 Ch#1    %4dmV\n\r", v1); Serial.print(buf);
       snprintf((char*)buf, sizeof(buf), "PCF8591 Ch#2    %4dmV\n\r", v2); Serial.print(buf);
       snprintf((char*)buf, sizeof(buf), "PCF8591 Ch#3    %4dmV\n\r", v3); Serial.print(buf);
-    } else { Serial.println(("PCF8591           NOT FOUND")); }
+    } else { Serial.println((F("PCF8591           NOT FOUND"))); }
 
     Serial.println(LINE_DOUBLE);
   }
