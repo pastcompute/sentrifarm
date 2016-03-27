3d1ffa1113a9e566a99d5d4af64dbfa3f6d9dab9
Andrew McDonnell <bugs@andrewmcdonnell.net>
2016-03-27 23:51:48 +1030

    Use onboard LED to indicate wifi reset sequence

--- software/mcu/sx1276-esp07/src/boot.cpp
+++ software/mcu/sx1276-esp07/src/boot.cpp
@@ -24,15 +24,32 @@ bool CheckForUserResetWifiOnBoot()
   if (ESP.getResetInfoPtr()->reason == REASON_DEFAULT_RST) {
     Serial.print(F("SF: Short GPIO0 now for 5 seconds, to reset WIFI data"));
     // TODO: turn on LED to indicate that button needs to be pressed..
+
+    yield();  delay(500); // give time for serial data to flush through
+    Serial.swap();
+    pinMode(1, OUTPUT);
+    digitalWrite(1, LOW); // Note, inverted sense
     delay(2000);
+
     int npress = 0;
-    while (digitalRead(0) == 0 && npress < 7) {
-      Serial.println(F("."));
+    while (digitalRead(0) == 0 && npress < 12) {
+      digitalWrite(1, HIGH);
+      delay(250);
+      digitalWrite(1, LOW);
+      delay(250);
       npress ++;
-      delay(300);
     }
-    if (npress >= 7) {
+    Serial.swap(); yield(); delay(500);
+    if (npress >= 12) {
       Serial.println(F("USER RESET of WIFI DATA. Reboot..."));
+      // Remain solid blue, to indicate remove the short
+      // and wait for the short to be removed
+      Serial.swap(); yield(); delay(500);
+      pinMode(1, OUTPUT);
+      digitalWrite(1, LOW);
+      while (digitalRead(0) == 0) {
+        delay(250);
+      }
       return false;
     }
   }
